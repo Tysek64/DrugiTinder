f4432b9c6942bd73de531124ab8b154ea1388d92
Tysek64 <74345334+Tysek64@users.noreply.github.com>
2025-11-12 11:48:23 +0100

    Delete SQL directory

--- SQL/z1.sql
+++
@@ -1,2 +0,0 @@
-\set countryName '''Poland'''
-SELECT "user".id, "user".name, "user".surname, city.name, country.name FROM "user" INNER JOIN city ON "user".fk_city_id = city.id INNER JOIN country ON city.fk_country_id = country.id WHERE country.name = :countryName;
